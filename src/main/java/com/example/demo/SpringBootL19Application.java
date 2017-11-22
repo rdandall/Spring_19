37384c521b6d77a98acffcc071bb073af34639bc
rdandall <robert@notebrush.com>
2017-11-21 20:58:54 -0500

    first commit

---
+++ src/main/java/com/example/demo/SpringBootL19Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootL19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootL19Application.class, args);
+	}
+}
